46d0c5a87361da52ea0c7d4849fc52e513dde13b
tallner <ctallner@hotmail.com>
2023-04-10 09:08:51 +0200

    test

--- data/dbcontext.go
+++ data/dbcontext.go
@@ -32,7 +32,7 @@ func InitDatabase(file, server, database, username, password string, port int) {
 	var antal int64
 	DB.Model(&Employee{}).Count(&antal) // Seed
 	if antal == 0 {
-		DB.Create(&Employee{Age: 50, Namn: "Stefan", City: "Test"})
+		DB.Create(&Employee{Age: 50, Namn: "Stefan", City: "Testar"})
 		DB.Create(&Employee{Age: 14, Namn: "Oliver", City: "Test"})
 		DB.Create(&Employee{Age: 20, Namn: "Josefine", City: "Test"})
 	}
